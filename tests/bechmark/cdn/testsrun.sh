9c37038ed68bd6158eb89235c87c011a221f2945
ddomizzisg <ddomizzisag@gmail.com>
2024-06-13 16:12:56 +0200

    tests

---
+++ tests/bechmark/cdn/testsrun.sh
@@ -0,0 +1,15 @@
+python3 main.py CDN --cdn 129.114.26.127:8095 --cdn-catalog ddbb0616acf60b098cfb34e6bcbdd3970bf1583fdb11320dc60243529185e502  --cdn-usertoken 574de8363e76b9e104b8e4b0b58d4e6e0250bc4c1f028140e08cc803f31e8d77 --ops SET --files /DATA/dsanchez/random/1M  --repeat=10 --clients 1  --resiliency 0 --parallel 1  --csv-file aws_lustre_nozip_no_res.cs
+
+python3 main.py CDN --cdn 129.114.26.127:8095 --cdn-catalog ddbb0616acf60b098cfb34e6bcbdd3970bf1583fdb11320dc60243529185e502  --cdn-usertoken 574de8363e76b9e104b8e4b0b58d4e6e0250bc4c1f028140e08cc803f31e8d77 --ops SET --files /DATA/dsanchez/random/1M  --repeat=10 --clients 1  --resiliency 1 --parallel 1  --csv-file aws_lustre_nozip_res.cs
+
+python3 main.py CDN --cdn 129.114.26.127:8095 --cdn-catalog ddbb0616acf60b098cfb34e6bcbdd3970bf1583fdb11320dc60243529185e502  --cdn-usertoken 574de8363e76b9e104b8e4b0b58d4e6e0250bc4c1f028140e08cc803f31e8d77 --ops SET --files /DATA/dsanchez/random/10M  --repeat=10 --clients 1  --resiliency 0 --parallel 1  --csv-file aws_lustre_nozip_no_res.cs
+
+python3 main.py CDN --cdn 129.114.26.127:8095 --cdn-catalog ddbb0616acf60b098cfb34e6bcbdd3970bf1583fdb11320dc60243529185e502  --cdn-usertoken 574de8363e76b9e104b8e4b0b58d4e6e0250bc4c1f028140e08cc803f31e8d77 --ops SET --files /DATA/dsanchez/random/10M  --repeat=10 --clients 1  --resiliency 1 --parallel 1  --csv-file aws_lustre_nozip_res.cs
+
+python3 main.py CDN --cdn 129.114.26.127:8095 --cdn-catalog ddbb0616acf60b098cfb34e6bcbdd3970bf1583fdb11320dc60243529185e502  --cdn-usertoken 574de8363e76b9e104b8e4b0b58d4e6e0250bc4c1f028140e08cc803f31e8d77 --ops SET --files /DATA/dsanchez/random/100M  --repeat=10 --clients 1  --resiliency 0 --parallel 1  --csv-file aws_lustre_nozip_no_res.cs
+
+python3 main.py CDN --cdn 129.114.26.127:8095 --cdn-catalog ddbb0616acf60b098cfb34e6bcbdd3970bf1583fdb11320dc60243529185e502  --cdn-usertoken 574de8363e76b9e104b8e4b0b58d4e6e0250bc4c1f028140e08cc803f31e8d77 --ops SET --files /DATA/dsanchez/random/100M  --repeat=10 --clients 1  --resiliency 1 --parallel 1  --csv-file aws_lustre_nozip_res.cs
+
+python3 main.py CDN --cdn 129.114.26.127:8095 --cdn-catalog ddbb0616acf60b098cfb34e6bcbdd3970bf1583fdb11320dc60243529185e502  --cdn-usertoken 574de8363e76b9e104b8e4b0b58d4e6e0250bc4c1f028140e08cc803f31e8d77 --ops SET --files /DATA/dsanchez/random/1000M  --repeat=1 --clients 1  --resiliency 0 --parallel 1  --csv-file aws_lustre_nozip_no_res.cs
+
+python3 main.py CDN --cdn 129.114.26.127:8095 --cdn-catalog ddbb0616acf60b098cfb34e6bcbdd3970bf1583fdb11320dc60243529185e502  --cdn-usertoken 574de8363e76b9e104b8e4b0b58d4e6e0250bc4c1f028140e08cc803f31e8d77 --ops SET --files /DATA/dsanchez/random/1000M  --repeat=1 --clients 1  --resiliency 1 --parallel 1  --csv-file aws_lustre_nozip_res.cs
